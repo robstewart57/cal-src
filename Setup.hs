c9165159488c19de7393d72c07fd55bd4e4dab31
Rob Stewart <robstewart57@gmail.com>
2015-06-08 22:50:22 +0100

    CAL components generated using BNFC as a library, not an executable

--- Setup.hs
+++ Setup.hs
@@ -1,18 +1,20 @@
 
-import Control.Monad (void)
 import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.Setup (BuildFlags)
-import System.Process
 
+import BNFC.GetCF
+import BNFC.Options
+import BNFC.Backend.Base (writeFiles)
+import BNFC.Backend.Haskell
+
+main :: IO ()
 main = defaultMainWithHooks simpleUserHooks { preBuild = calGenHook }
 
 calGenHook :: Args -> BuildFlags -> IO HookedBuildInfo
 calGenHook _ _ = do
-  -- currently just uses the `bnfc` executable, a better
-  -- approach would be the use BNFC as a library.
-  -- See: https://groups.google.com/forum/#!topic/bnfc-dev/9_37G02ErDw
-  --
-  -- For this reason, this is currently not in a fit state for hackage.
-  void $ createProcess (proc "bnfc" ["-m","--haskell","CAL.cf"])
+  let opts = defaultOptions { lang = "CAL" }
+  grammar <- readFile "CAL.cf"
+  cf <- parseCF opts TargetHaskell grammar
+  writeFiles "." $ makeHaskell opts cf
   return emptyHookedBuildInfo
