a41227030c424919d864775a4eda761eb796076d
Rob Stewart <robstewart57@gmail.com>
2017-01-26 21:29:53 +0000

    More playing with Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -5,16 +5,19 @@ import Distribution.Simple
 import Distribution.Simple.Setup (BuildFlags)
 import System.Process
 
-main = defaultMainWithHooks simpleUserHooks { preBuild = calGenHook }
+main = do
+  void $ createProcess (proc "bnfc" ["-m","--haskell","CAL.cf"])
+  defaultMain
+  -- defaultMainWithHooks simpleUserHooks { preBuild = calGenHook }
 
-calGenHook :: Args -> BuildFlags -> IO HookedBuildInfo
-calGenHook _ _ = do
+-- calGenHook :: Args -> BuildFlags -> IO HookedBuildInfo
+-- calGenHook _ _ = do
   -- currently just uses the `bnfc` executable, a better
   -- approach would be the use BNFC as a library.
   -- See: https://groups.google.com/forum/#!topic/bnfc-dev/9_37G02ErDw
   --
   -- For this reason, this is currently not in a fit state for hackage.
-  void $ createProcess (proc "bnfc" ["-m","--haskell","CAL.cf"])
-  void $ createProcess (proc "happy" ["-gca","ParCAL.y"])
-  void $ createProcess (proc "alex" ["-g","LexCAL.x"])
-  return emptyHookedBuildInfo
+  -- void $ createProcess (proc "bnfc" ["-m","--haskell","CAL.cf"])
+  -- void $ createProcess (proc "happy" ["-gca","ParCAL.y"])
+  -- void $ createProcess (proc "alex" ["-g","LexCAL.x"])
+  -- return emptyHookedBuildInfo
